a4661b0fd464609d2abbb834107dd1f3194cb86c
LyAhn <jaybentham@gmail.com>
2024-07-06 11:44:18 +0100

    Feature batchconvert (#4)
    * Adds batch converter
    
    - Adds initial batch converter
    - Issues with JPEG batch (removed functionality)
    
    * Fix JPG conversion
    
    - Fixes JPEG Batch conversion
    - Fixes unsupported files breaking specifically JPEG batches

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 import os
 from tkinter import filedialog, messagebox
 
@@ -10,7 +10,7 @@ class ImageNexus(ttk.Window):
     def __init__(self):
         super().__init__(themename="darkly")
         self.title("ImageNexus v{}".format(version))
-        self.geometry("650x400")
+        self.geometry("650x475")
         self.resizable(False, False)
         # set window icon
         #self.iconbitmap("icon.ico")
@@ -25,12 +25,15 @@ class ImageNexus(ttk.Window):
         # Create tabs
         self.extractor_tab = ttk.Frame(self.notebook)
         self.converter_tab = ttk.Frame(self.notebook)
+        self.batch_converter_tab = ttk.Frame(self.notebook)
 
         self.notebook.add(self.extractor_tab, text="Frame Extractor")
         self.notebook.add(self.converter_tab, text="Image Converter")
+        self.notebook.add(self.batch_converter_tab, text="Batch Converter")
 
         self.create_extractor_widgets()
         self.create_converter_widgets()
+        self.create_batch_converter_widgets()
 
     def create_extractor_widgets(self):
         main_frame = ttk.Frame(self.extractor_tab, padding="20 20 20 20")
@@ -49,6 +52,11 @@ class ImageNexus(ttk.Window):
         self.file_type_var = ttk.StringVar(value="PNG")
         file_type_combo = ttk.Combobox(file_type_frame, textvariable=self.file_type_var, values=["PNG", "GIF"], width=10, state="readonly")
         file_type_combo.pack(side=LEFT, padx=(10, 0))
+        
+        # Generate frame info file checkbox
+        self.generate_frame_info_var = ttk.BooleanVar(value=True)
+        generate_frame_info_check = ttk.Checkbutton(main_frame, text="Generate frame info file", variable=self.generate_frame_info_var)
+        generate_frame_info_check.pack(pady=(10, 0))
 
         # Extract button
         ttk.Button(main_frame, text="Extract Frames", command=self.extract_frames, style='success.TButton').pack(pady=20)
@@ -58,7 +66,7 @@ class ImageNexus(ttk.Window):
         self.progress.pack(pady=10)
 
         # Status label
-        self.status_label = ttk.Label(main_frame, text="", font=("TkDefaultFont", 10))
+        self.status_label = ttk.Label(main_frame, text="", font=("TkDefaultFont", 10), wraplength=400, justify="left")
         self.status_label.pack(pady=10)
 
     def create_converter_widgets(self):
@@ -72,15 +80,44 @@ class ImageNexus(ttk.Window):
         self.create_labeled_entry(main_frame, "Output Folder:", self.select_output_folder_converter, "output_folder_converter_entry")
 
         # Output format selection
-        formats = ["GIF", "PNG", "JPG", "BMP", "TIFF"]
+        formats = ["GIF", "PNG", "JPEG", "BMP", "TIFF"]
         self.create_format_selection(main_frame, "Output Format:", "output_format_var", formats)
 
         # Convert button
         ttk.Button(main_frame, text="Convert File", command=self.convert_file, style='success.TButton').pack(pady=20)
 
         # Converter status label
-        self.converter_status_label = ttk.Label(main_frame, text="", font=("TkDefaultFont", 10))
+        self.converter_status_label = ttk.Label(main_frame, text="", font=("TkDefaultFont", 10), wraplength=400, justify="left")
         self.converter_status_label.pack(pady=10)
+
+    def create_batch_converter_widgets(self):
+        main_frame = ttk.Frame(self.batch_converter_tab, padding="20 20 20 20")
+        main_frame.pack(fill=BOTH, expand=YES)
+
+    # Conversion type selection
+        conversion_type_frame = ttk.Frame(main_frame)
+        conversion_type_frame.pack(fill=X, pady=10)
+        ttk.Label(conversion_type_frame, text="Conversion Type:", width=15).pack(side=LEFT)
+        self.conversion_type_var = ttk.StringVar(value="Files")
+        conversion_type_combo = ttk.Combobox(conversion_type_frame, textvariable=self.conversion_type_var, values=["Files", "Folder"], width=10, state="readonly")
+        conversion_type_combo.pack(side=LEFT, padx=(10, 0))
+
+    # Input selection
+        self.create_labeled_entry(main_frame, "Select Input:", self.select_batch_input, "batch_input_entry")
+
+    # Output folder selection
+        self.create_labeled_entry(main_frame, "Output Folder:", self.select_output_folder_batch, "output_folder_batch_entry")
+
+    # Output format selection
+        formats = ["GIF", "PNG", "JPEG", "BMP", "TIFF"]
+        self.create_format_selection(main_frame, "Output Format:", "output_format_var_batch", formats)
+
+    # Convert button
+        ttk.Button(main_frame, text="Convert Files", command=self.convert_batch, style='success.TButton').pack(pady=20)
+
+    # Batch converter status label
+        self.batch_converter_status_label = ttk.Label(main_frame, text="", font=("TkDefaultFont", 10), wraplength=400, justify="left")
+        self.batch_converter_status_label.pack(pady=10)
 
     def create_labeled_entry(self, parent, label_text, command, entry_name):
         frame = ttk.Frame(parent)
@@ -104,8 +141,8 @@ class ImageNexus(ttk.Window):
         self.select_file(self.gif_path_entry, [("GIF files", "*.gif")])
 
     def select_input_file(self):
-        #self.select_file(self.input_file_entry, [("Image files", "*.gif;*.png;*.jpg;*.jpeg;*.bmp;*.tiff")])
-        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.gif;*.png;*.jpg;*.jpeg;*.bmp;*.tiff")])
+        #self.select_file(self.input_file_entry, [("Image files", "*.gif;*.png;*.JPEG;*.jpeg;*.bmp;*.tiff")])
+        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.gif;*.png;*.JPEG;*.jpeg;*.bmp;*.tiff")])
         if file_path:
             self.input_file_entry.delete(0, END)
             self.input_file_entry.insert(0, file_path)
@@ -133,6 +170,7 @@ class ImageNexus(ttk.Window):
         gif_path = self.gif_path_entry.get()
         output_folder = self.output_folder_entry.get()
         file_type = self.file_type_var.get().lower()
+        generate_frame_info = self.generate_frame_info_var.get()
 
         if not gif_path or not output_folder:
             messagebox.showerror("Error", "Please select both GIF file and output folder.")
@@ -176,7 +214,8 @@ class ImageNexus(ttk.Window):
                     self.update_idletasks()
 
                 # Generate frame info text file
-                self.generate_frame_info_file(output_folder, frame_info)
+                if generate_frame_info:
+                    self.generate_frame_info_file(output_folder, frame_info)
 
             self.status_label['text'] = "Frames extracted successfully!"
             messagebox.showinfo("Success", "Frames extracted successfully!")
@@ -196,6 +235,92 @@ class ImageNexus(ttk.Window):
             f.write(f"FPS: {fps:.2f}\n\n")
             for frame, duration, total_time in frame_info:
                 f.write(f"Frame {frame:03d}: Duration = {duration:.3f}s, Total Time = {total_time:.3f}s\n")
+
+    def select_batch_input(self):
+        conversion_type = self.conversion_type_var.get()
+        if conversion_type == "Files":
+            file_paths = filedialog.askopenfilenames(filetypes=[("Image files", "*.gif;*.png;*.bmp;*.tiff")])
+            self.batch_input_entry.delete(0, END)
+            self.batch_input_entry.insert(0, ", ".join(file_paths))
+        elif conversion_type == "Folder":
+            folder_path = filedialog.askdirectory()
+            self.batch_input_entry.delete(0, END)
+            self.batch_input_entry.insert(0, folder_path)
+
+    def select_output_folder_batch(self):
+        folder_path = filedialog.askdirectory()
+        if folder_path:
+            self.output_folder_batch_entry.delete(0, END)
+            self.output_folder_batch_entry.insert(0, folder_path)
+
+    def convert_batch(self):
+        input_paths = self.batch_input_entry.get()
+        output_folder = self.output_folder_batch_entry.get()
+        output_format = self.output_format_var_batch.get().lower()
+
+        if not input_paths or not output_folder:
+            messagebox.showerror("Error", "Please select input files/folder and output folder.")
+            return
+
+        conversion_type = self.conversion_type_var.get()
+        if conversion_type == "Files":
+            input_paths = input_paths.split(", ")
+            self.convert_batch_files(input_paths, output_folder, output_format)
+        elif conversion_type == "Folder":
+            self.convert_batch_folder(input_paths, output_folder, output_format)
+
+    def convert_jpeg_mode(self, img):
+        if img.mode == 'RGB':
+            img = img.convert('RGB')
+        elif img.mode == 'RGBA':
+            img = img.convert('RGB')
+        elif img.mode == 'LA':
+            img = img.convert('L')
+        return img
+
+    def convert_batch_folder(self, folder_path, output_folder, output_format):
+        image_extensions = ['.gif', '.png', '.jpg', '.jpeg', '.bmp', '.tiff']
+        supported_formats = ['GIF', 'PNG', 'JPEG', 'BMP', 'TIFF']
+
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                input_path = os.path.join(root, file)
+                extension = os.path.splitext(input_path)[1].lower()
+                input_format = extension[1:].upper()
+
+                if extension in image_extensions:
+                    if input_format in supported_formats:
+                        try:
+                            with Image.open(input_path) as img:
+                                if input_format == 'GIF' and output_format != 'GIF':
+                                    img.seek(0)
+
+                                # Convert image mode if necessary
+                                if output_format == 'jpeg':
+                                    img = self.convert_jpeg_mode(img)
+                                elif output_format == 'BMP':
+                                    if img.mode != 'RGB':
+                                        img = img.convert('RGB')
+
+                                output_filename = os.path.splitext(file)[0] + f".{output_format}"
+                                output_path = os.path.join(output_folder, output_filename)
+
+                                img.save(output_path, format=output_format)
+
+                            self.batch_converter_status_label['text'] = f"File {input_path} converted successfully!"
+                        except UnidentifiedImageError:
+                            self.batch_converter_status_label['text'] = f"Skipping invalid image file: {input_path}"
+                        except Exception as e:
+                            self.batch_converter_status_label['text'] = f"Error converting {input_path}: {str(e)}"
+                    else:
+                        self.batch_converter_status_label['text'] = f"Skipping unsupported file: {input_path}"
+                elif extension.lower() not in ['.exe', '.dll', '.sys', '.bat', '.cmd']:
+                    self.batch_converter_status_label['text'] = f"Skipping unsupported file: {input_path}"
+                else:
+                # Ignore system files and executables
+                    pass
+
+        messagebox.showinfo("Success", "Batch conversion completed!")
 
     def convert_file(self):
         input_path = self.input_file_entry.get()
@@ -224,12 +349,14 @@ class ImageNexus(ttk.Window):
                     # If converting from GIF to non-GIF, use only the first frame
                     img.seek(0)
 
-                # Generate output filename
-                input_filename = os.path.basename(input_path)
-                output_filename = os.path.splitext(input_filename)[0] + f".{output_format}"
-                output_path = os.path.join(output_folder, output_filename)
-
-                img.save(output_path, format=output_format)
+                    # Convert to RGB if necessary
+                    if output_format == 'jpeg':
+                        if img.mode == 'RGBA':
+                            img = img.convert('RGB')
+                    elif output_format == 'bmp':
+                        if img.mode != 'RGB':
+                            img = img.convert('RGB')
+                img.save(output_path, format=output_format.upper())
 
             self.converter_status_label['text'] = "Image converted successfully!"
             messagebox.showinfo("Success", "Image converted successfully!")
@@ -237,6 +364,9 @@ class ImageNexus(ttk.Window):
             self.converter_status_label['text'] = f"Error: {str(e)}"
             messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
+
+
 if __name__ == "__main__":
     app = ImageNexus()
     app.mainloop()
+    print(f"The Applications __name__ was {__name__}")
